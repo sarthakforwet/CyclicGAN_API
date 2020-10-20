647aa84f6d0e084297d8dd3ce3b7f5d8f1aa612d
sarthakforwet <sarthakkhandelwal032000@gmail.com>
2020-10-20 12:17:15 +0530

    Modified build file

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup
+from setuptools import find_packages
 
-setup(name="CyclicGAN",
-version=0.2,
-description="An Implementation of CyclicGAN",
-author="sarthak",
-packages=['CyclicGAN'],
-zip_safe=False)
+with open("Readme.md") as f:
+    long_description = f.read()
+
+setup(
+    name="cyclicgan",
+    version="0.0.1",
+    description="An API implementation of CyclicGAN network.",
+    author="Sarthak Khandelwal",
+    author_email="sarthakkhandelwal032000@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sarthakforwet/CyclicGAN_Implementation",
+    packages=find_packages(),
+    python_requires= '>=3.6',
+    )
